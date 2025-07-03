b9118686ede60dcfc276a88971578ac5ec788509
1F10240176 <s1f102401764@iniad.org>
2025-07-03 13:09:51 +0900

    詳細ページのテストを追加

--- todo/views.py
+++ todo/views.py
@@ -25,8 +25,8 @@ def index(request):
 def detail(request, task_id):
     try:
         task = Task.objects.get(pk=task_id)
-    except Task.DoseNotExist:
-        raise Http404("Task dose not exist")
+    except Task.DoesNotExist:
+        raise Http404("Task does not exist")
     
     context = {
         'task': task,
